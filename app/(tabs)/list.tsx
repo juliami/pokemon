7ce31bf030831306acdc87d64fc402118cbea988
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-08 12:55:44 +0100

    Initial tabs setup

---
+++ app/(tabs)/list.tsx
@@ -0,0 +1,10 @@
+import { SimpleView } from '@/components/simple-view';
+import { ThemedText, } from '@/components/themed-text';
+
+const ListScreen = () => (
+    <SimpleView>
+        <ThemedText type="defaultSemiBold">List goes here</ThemedText>
+    </SimpleView>
+);
+
+export default ListScreen;
---
+++ components/simple-view.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, type ViewProps } from 'react-native';
+
+export function SimpleView({ ...props }: ViewProps) {
+  return <View style={[styles.simpleView]} {...props} />;
+}
+
+const styles = StyleSheet.create({
+  simpleView: {
+    flex: 1,
+    padding: 32,
+    gap: 16,
+    overflow: 'hidden',
+  },
+});
+
