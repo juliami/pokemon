5a1bdaf662d10e5096ccdd2f13fb1afafe2289ca
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-08 16:38:00 +0100

    Remove unrelated change

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+
 export const unstable_settings = {
   anchor: "(tabs)",
 };
