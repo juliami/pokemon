78a6effad1c4999071d843deb27d84127506ce10
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-08 16:28:17 +0100

    Display Pokemon list - initial version

---
+++ components/pokemon-list-item.tsx
@@ -0,0 +1,33 @@
+
+import { Pokemon } from "@/types";
+import { StyleSheet, View } from "react-native";
+import { ThemedText } from "./themed-text";
+
+interface PokemonListItemProps extends Pokemon {
+    index: number;
+}
+
+const PokemonListItem = ({ name, id, index }: PokemonListItemProps) => {
+    return (<View style={styles.listItem}>
+        <ThemedText type="default">{index}</ThemedText>
+        <ThemedText>{name}</ThemedText>
+        <ThemedText style={styles.id}>#{id}</ThemedText>
+    </View>)
+}
+
+
+const styles = StyleSheet.create({
+    listItem: {
+        display: 'flex',
+        padding: 8,
+        flexDirection: 'row',
+        gap: 4,
+
+    },
+    id: {
+        marginLeft: 'auto'
+    }
+});
+
+
+export default PokemonListItem;
---
+++ components/pokemon-list.tsx
@@ -0,0 +1,64 @@
+
+import { getPokemons } from "@/api/fetch";
+import { Pokemon } from "@/types";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text } from 'react-native';
+import PokemonListItem from "./pokemon-list-item";
+import { ThemedText } from "./themed-text";
+
+const PokemonList = () => {
+
+    const [pokemons, setPokemons] = useState<Pokemon[] | undefined>(undefined);
+
+    useEffect(() => {
+        // todo: move logic to the separate hook, handle loading and error states
+        const fetchData = async () => {
+            const data = await getPokemons()
+            setPokemons(data);
+        };
+        fetchData();
+    }, []);
+
+    if (!pokemons) {
+        return <ThemedText type="defaultSemiBold">Loading...</ThemedText>
+    }
+
+    return (
+        <>
+            <ThemedText type="defaultSemiBold" style={styles.header}>
+                <Text>No.</Text>
+                <Text>Name</Text>
+                <Text style={styles.id}>Id</Text>
+
+            </ThemedText>
+
+            <FlatList
+                data={pokemons}
+                renderItem={({ item, index }) => <PokemonListItem name={item.name} id={item.id} index={index} />}
+                keyExtractor={item => item.name}
+                style={styles.list}
+            />
+        </>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    list: {
+        display: 'flex',
+    },
+    header: {
+        display: 'flex',
+        padding: 8,
+        flexDirection: 'row',
+        gap: 4,
+
+    },
+    id: {
+        marginLeft: 'auto'
+    }
+});
+
+
+
+export default PokemonList;
