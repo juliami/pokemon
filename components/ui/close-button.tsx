ec17b8b1e64e7d5ce2ab733356c9288ec1442e89
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-13 13:23:34 +0100

    Add close button

---
+++ components/ui/close-button.tsx
@@ -0,0 +1,15 @@
+import { Platform, Pressable } from "react-native";
+import { IconSymbol } from "./icon-symbol";
+
+const CloseButton = ({ onPress }: { onPress: () => void }) => {
+  if (Platform.OS === "web") {
+    return null;
+  }
+  return (
+    <Pressable onPress={onPress}>
+      <IconSymbol size={32} name={"xmark"} color={"#fff"} weight="medium" />
+    </Pressable>
+  );
+};
+
+export default CloseButton;
