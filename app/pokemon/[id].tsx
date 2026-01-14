0bc9add5d13bbbffcd77143616921b6ed9c3c9f3
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-14 10:23:45 +0100

    Optimize fetching

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,27 +1,16 @@
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import Loading from "@/components/loading";
 import Pokemon from "@/components/pokemon";
-import { useGetPokemonByIdQuery } from "@/hooks/use-get-pokemon-by-id";
-import { capitalizeFirstLetter } from "@/utils/text";
-import { useEffect } from "react";
 
-export default function PokemonDetailsScreen() {
+const PokemonDetailsScreen = () => {
   const { id } = useLocalSearchParams();
-  const { data: pokemon, loading } = useGetPokemonByIdQuery(Number(id));
-  const navigation = useNavigation();
-
-
-  useEffect(() => {
-    navigation.setOptions({
-      title: pokemon?.name ? capitalizeFirstLetter(pokemon.name) : "Loading...",
-    });
-  }, [navigation, pokemon]);
 
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
-      {loading || !pokemon ? <Loading /> : <Pokemon id={id.toString()} />}
+      <Pokemon id={id.toString()} showCloseButton={true} />
     </SafeAreaView>
   );
-}
+};
+
+export default PokemonDetailsScreen;
