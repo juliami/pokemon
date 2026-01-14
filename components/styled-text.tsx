5f3bf9ca110529d8c8fe292f23bc849008c45f8b
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-14 16:32:00 +0100

    Fix Android text alignment issue

--- components/styled-text.tsx
+++ components/styled-text.tsx
@@ -15,6 +15,7 @@ export function StyledText({
     <Text
       style={[
         { color: darkColor },
+        {fontFamily: 'Slate'},
         type === "default" ? styles.default : undefined,
         type === "title" ? styles.title : undefined,
         type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
