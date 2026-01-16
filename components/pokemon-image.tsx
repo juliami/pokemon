7b211ae68615996d9c7331b0199f6e63452c1060
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-16 10:20:23 +0100

    Optimize PokemonListItem rendering

--- components/pokemon-image.tsx
+++ components/pokemon-image.tsx
@@ -9,8 +9,7 @@ type Props = {
 };
 
 const blurhash =
-  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 const PokemonImage = ({
   imageUri,
