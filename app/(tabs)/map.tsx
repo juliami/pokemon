810ff467c1eae9bb885588cc3de3fad44a4d2eb3
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-16 15:49:53 +0100

    Open/close BottomSheet on Marker selection

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,6 +1,10 @@
+import Pokemon from "@/components/pokemon";
 import { useMapMarkersContext } from "@/hooks/use-map-markers-context";
 import { getRandomPokemonID } from "@/utils/get-random-pokemon-id";
-import React from "react";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import React, { useMemo, useRef, useState } from "react";
+import { StyleSheet, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import MapView, { LatLng, Marker } from "react-native-maps";
 
 interface PokemonMarker extends LatLng {
@@ -8,9 +12,14 @@ interface PokemonMarker extends LatLng {
 }
 
 const MapScreen = () => {
-  const { markers, addMarker, removeMarker } = useMapMarkersContext();
+  const { markers, addMarker } = useMapMarkersContext();
+  const sheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["5%", "40%"], []);
+  const [selectedPokemonId, setSelectedPokemonId] = useState<number | null>(
+    null,
+  );
 
-  const handleMapLongClick = (event: {
+  const handleMapLongPress = (event: {
     nativeEvent: { coordinate: LatLng };
   }) => {
     const pokemonId = getRandomPokemonID();
@@ -18,21 +27,64 @@ const MapScreen = () => {
     addMarker({ ...coordinate, pokemonId });
   };
 
-  const handleMarkerPress = (marker: PokemonMarker) => {
-    console.log("Marker pressed, Pokémon ID:", marker.pokemonId);
+  const handleMarkerSelect = (marker: PokemonMarker) => {
+    setSelectedPokemonId(marker.pokemonId);
+    sheetRef.current?.expand();
+  };
+
+  const closeBottomSheet = () => {
+    sheetRef.current?.close();
   };
 
   return (
-    <MapView style={{ flex: 1 }} onLongPress={handleMapLongClick}>
-      {markers.map((marker, index) => (
-        <Marker
-          key={index}
-          coordinate={marker}
-          onPress={() => handleMarkerPress(marker)}
-        />
-      ))}
-    </MapView>
+    <>
+      <GestureHandlerRootView style={styles.container}>
+        <MapView
+          style={{ flex: 1 }}
+          onLongPress={handleMapLongPress}
+          onPress={(e) => closeBottomSheet(e)}
+        >
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={marker}
+              onSelect={handleMarkerSelect}
+              stopPropagation={true}
+            />
+          ))}
+        </MapView>
+        <BottomSheet
+          ref={sheetRef}
+          index={-1}
+          snapPoints={snapPoints}
+          enablePanDownToClose={true}
+          animateOnMount={true}
+        >
+          <BottomSheetView style={styles.sheetContent}>
+            {selectedPokemonId ? (
+              <Pokemon
+                id={selectedPokemonId.toString()}
+                showCloseButton={false}
+                showPokemonData={false}
+              />
+            ) : (
+              <Text style={styles.title}>No Pokémon selected</Text>
+            )}
+          </BottomSheetView>
+        </BottomSheet>
+      </GestureHandlerRootView>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: { flex: 1 },
+  sheetContent: {
+    flex: 1,
+  },
+  title: { fontSize: 18, fontWeight: "bold", marginBottom: 10 },
+  subtitle: { fontSize: 16, color: "#555" },
+});
 
 export default MapScreen;
