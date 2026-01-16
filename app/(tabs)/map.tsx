172eb2506e8b3b231e1814000bc03fa2c325ab03
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-16 16:45:52 +0100

    Deselect marker when bottom sheet is closed

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,23 +1,24 @@
 import Pokemon from "@/components/pokemon";
 import PokemonMarker from "@/components/pokemon-marker";
+import { PokemonMarkerType } from "@/context/map-markers";
 import { useMapMarkersContext } from "@/hooks/use-map-markers-context";
 import { getRandomPokemonID } from "@/utils/get-random-pokemon-id";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import React, { useMemo, useRef, useState } from "react";
 import { StyleSheet, Text } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import MapView, { LatLng } from "react-native-maps";
-
-interface PokemonMarker extends LatLng {
-  pokemonId: number;
-}
+import MapView, { LatLng, Marker } from "react-native-maps";
 
 const MapScreen = () => {
   const { markers, addMarker } = useMapMarkersContext();
+
   const sheetRef = useRef<BottomSheet>(null);
+  const selectedMarkerRef = useRef<InstanceType<typeof Marker> | null>(null);
+
   const snapPoints = useMemo(() => ["5%", "40%"], []);
+
   const [selectedPokemonId, setSelectedPokemonId] = useState<number | null>(
-    null,
+    null
   );
 
   const handleMapLongPress = (event: {
@@ -28,9 +29,15 @@ const MapScreen = () => {
     addMarker({ ...coordinate, pokemonId });
   };
 
-  const handleMarkerSelect = (marker: PokemonMarker) => {
+  const handleMarkerSelect = (marker: PokemonMarkerType) => {
     setSelectedPokemonId(marker.pokemonId);
     sheetRef.current?.expand();
+  };
+
+  const handleMarkerDeselection = () => {
+             selectedMarkerRef.current?.hideCallout();
+            selectedMarkerRef.current = null;
+            setSelectedPokemonId(null);
   };
 
   const closeBottomSheet = () => {
@@ -49,9 +56,17 @@ const MapScreen = () => {
             <PokemonMarker
               pokemonId={marker.pokemonId}
               key={index}
-              coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
-              onSelect={()=>handleMarkerSelect(marker)}
+              coordinate={{
+                latitude: marker.latitude,
+                longitude: marker.longitude,
+              }}
+              onSelect={() => handleMarkerSelect(marker)}
               stopPropagation={true}
+              ref={(ref) => {
+                if (marker.pokemonId === selectedPokemonId) {
+                  selectedMarkerRef.current = ref;
+                }
+              }}
             />
           ))}
         </MapView>
@@ -61,6 +76,7 @@ const MapScreen = () => {
           snapPoints={snapPoints}
           enablePanDownToClose={true}
           animateOnMount={true}
+          onClose={handleMarkerDeselection}
         >
           <BottomSheetView style={styles.sheetContent}>
             {selectedPokemonId ? (
