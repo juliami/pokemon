e662ce1eef680c91489d2cab249ebb06e2640cc1
Bartlomiej Fryz <bartlomiej.fryz@ext.chime.com>
2026-01-20 15:35:58 +0100

    Fixes ios theme observer native implementation

--- modules/system-theme/ios/SystemThemeModule.swift
+++ modules/system-theme/ios/SystemThemeModule.swift
@@ -5,6 +5,9 @@ public class SystemThemeModule: Module {
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
+    
+  private var themeObserver: Any?
+    
   public func definition() -> ModuleDefinition {
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
@@ -17,29 +20,29 @@ public class SystemThemeModule: Module {
       return getSystemTheme()
     }
 
-    OnStartObserving {
-      NotificationCenter.default.addObserver(
-        forName: NSNotification.Name("UserInterfaceStyleDidChange"),
-        object: nil,
-        queue: .main
-      ) { [weak self] _ in
-        self?.didChangeSystemTheme()
-      }
+   OnStartObserving {
+      themeObserver = NotificationCenter.default.addObserver(
+        forName: NSNotification.Name.RCTUserInterfaceStyleDidChange, object: nil, queue: nil, using: didChangeSystemTheme
+      )
     }
 
     OnStopObserving {
-      NotificationCenter.default.removeObserver(self, name: NSNotification.Name("UserInterfaceStyleDidChange"), object: nil)
+        if let observer = themeObserver {
+            // TODO: check if works with many observing hooks
+            NotificationCenter.default.removeObserver(observer)
+            themeObserver = nil
+        }
     }
+  }
 
-    @objc func didChangeSystemTheme() {
+  @objc func didChangeSystemTheme(notification: Notification) {
       self.sendEvent("onSystemThemeChange", [
         "theme": getSystemTheme()
       ])
     }
 
-    func getSystemTheme() -> String {
-      let style = UIScreen.main.traitCollection.userInterfaceStyle  
-      return style == .dark ? "DARK" : "LIGHT"   
-    }
+  func getSystemTheme() -> String {
+    let style = UIScreen.main.traitCollection.userInterfaceStyle  
+    return style == .dark ? "DARK" : "LIGHT"   
   }
 }
