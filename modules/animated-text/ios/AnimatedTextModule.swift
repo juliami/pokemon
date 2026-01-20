04bd5b0f54336f2753de8078416cde98e985e4fd
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-20 13:01:33 +0100

    WIP - dark mode

--- modules/animated-text/ios/AnimatedTextModule.swift
+++ modules/animated-text/ios/AnimatedTextModule.swift
@@ -1,11 +1,20 @@
 import ExpoModulesCore
 
 public class AnimatedTextModule: Module {
+
+  func notifyThemeChange() {
+    sendEvent("onChangeTheme", [
+      "theme": "changed"
+    ])
+  }
+  
     public func definition() -> ModuleDefinition {
         Name("AnimatedText") // JS name
 
         View(AnimatedTextViewContainer.self) {
             // No props for now
         }
+
+        Events("onChangeTheme")
     }
 }
--- modules/animated-text/src/AnimatedTextModule.ts
+++ modules/animated-text/src/AnimatedTextModule.ts
@@ -1,6 +1,9 @@
 import { NativeModule, requireNativeModule } from "expo";
 
-import { AnimatedTextModuleEvents } from "./AnimatedText.types";
+import {
+  AnimatedTextModuleEvents,
+  ThemeChangeEvent,
+} from "./AnimatedText.types";
 
 declare class AnimatedTextModule extends NativeModule<AnimatedTextModuleEvents> {
   PI: number;
@@ -8,5 +11,15 @@ declare class AnimatedTextModule extends NativeModule<AnimatedTextModuleEvents> 
   setValueAsync(value: string): Promise<void>;
 }
 
+const NativeAnimatedTextModule =
+  requireNativeModule<AnimatedTextModule>("AnimatedText");
+
+NativeAnimatedTextModule.addListener(
+  "onChangeTheme",
+  (event: ThemeChangeEvent) => {
+    console.log("changed theme");
+  },
+);
+
 // This call loads the native module object from the JSI.
-export default requireNativeModule<AnimatedTextModule>("AnimatedText");
+export default NativeAnimatedTextModule;
