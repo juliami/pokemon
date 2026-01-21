98f6ce0ce5c0665eb6c139108ca8c4652fe9bf30
Julia Miodyńska <julia.miodynska@gmail.com>
2026-01-21 10:58:29 +0100

    Chore: cleanup build artifacts and update gitignore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import EmptyFavoritePokemonPlaceholder from "@/components/empty-fav-pokemon-placeholder";
 import Pokemon from "@/components/pokemon";
+
 import { SimpleView } from "@/components/simple-view";
 import { FavoritePokemonContext } from "@/context/favorite-pokemon";
 import React, { useContext } from "react";
